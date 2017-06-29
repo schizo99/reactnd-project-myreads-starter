f3b136efd550e567eb421c0e24990430d0d1bf8d
Andreas Ahman <andreas.ahman@ikea.com>
2017-06-29 14:41:44 +0200

    Removed unused property type

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -8,19 +8,19 @@ class Bookshelf extends Component {
         <div>
           <div className="bookshelf">
             <h2 className="bookshelf-title">Currently Reading</h2>
-              <Shelf type="currentlyReading" books={this.props.books.filter(book => book.shelf === "currentlyReading")}/>
+              <Shelf books={this.props.books.filter(book => book.shelf === "currentlyReading")}/>
           </div>
         </div>
         <div>
           <div className="bookshelf">
             <h2 className="bookshelf-title">Want To Read</h2>
-              <Shelf type="wantToRead" books={this.props.books.filter(book => book.shelf === "wantToRead")}/>
+              <Shelf books={this.props.books.filter(book => book.shelf === "wantToRead")}/>
           </div>
         </div>
         <div>
           <div className="bookshelf">
             <h2 className="bookshelf-title">Read</h2>
-              <Shelf type="read" books={this.props.books.filter(book => book.shelf === "read")}/>
+              <Shelf books={this.props.books.filter(book => book.shelf === "read")}/>
           </div>
         </div>
       </div>
