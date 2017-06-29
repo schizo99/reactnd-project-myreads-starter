6839a2907005c93b9e9cdf5c3347a89f39045a9e
Andreas Ahman <andreas.ahman@ikea.com>
2017-06-29 14:40:52 +0200

    Made it possible to search books using the BooksAPI

--- src/Search.js
+++ src/Search.js
@@ -1,22 +1,37 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-
+import * as BooksAPI from './BooksAPI'
+import Shelf from './Shelf'
 class Search extends Component {
-    render () {
-        return (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to="/">Close</Link>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
+  state = {
+    query: '',
+    books: []
+  }
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+    BooksAPI.search(query, 30).then(books => {this.setState({books})})
+  }
+  render () {
+      return (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link className="close-search" to="/">Close</Link>
+            <div className="search-books-input-wrapper">
+              <input 
+                type="text" 
+                placeholder="Search by title or author"
+                onChange={(e) => this.updateQuery(e.target.value)}
+              />
             </div>
           </div>
-        )
-    }
+          <div className="search-books-results">
+            <ol className="books-grid">
+              <Shelf books={this.state.books.filter(book => book.shelf === "none")}/>
+            </ol>
+          </div>
+        </div>
+      )
+  }
 }
 
 export default Search
