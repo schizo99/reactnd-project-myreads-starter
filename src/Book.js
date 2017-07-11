4ccc5a7d44478b318d9a21e92651c84e9f9c33d0
Andreas Ahman <andreas.ahman@ikea.com>
2017-07-11 11:17:53 +0200

    Removed the header on books

--- src/Book.js
+++ src/Book.js
@@ -24,19 +24,18 @@ class Book extends Component {
       <div className="book">
        <div className="book-top">
          <div className="book-cover" style={{ height: 192, width: 128, backgroundImage: `url(${book.imageLinks !== undefined ? book.imageLinks.thumbnail: ''})` }}></div>
-         <div className="book-shelf">{book.shelf !== 'none' && book.shelf}</div>
-         <div className="book-shelf-changer">
-           <select value={book.shelf} onChange={(e) => this.updateBook(e.target.value)}>
-             <option value="" disabled>Move to...</option>
-             <option value="currentlyReading">Currently Reading</option>
-             <option value="wantToRead">Want to Read</option>
-             <option value="read">Read</option>
-             <option value="none">None</option>
-           </select>
-         </div>
+          <div className="book-shelf-changer">
+            <select value={book.shelf} onChange={(e) => this.updateBook(e.target.value)}>
+              <option value="" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
        </div>
-       <div className="book-title">{book.title}</div>
-       <div className="book-authors">{book.authors}</div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
       </div>
     )
   }
