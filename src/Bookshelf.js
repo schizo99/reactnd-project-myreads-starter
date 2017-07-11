9fa4df13fc16bf19a2f7555a8028b07018b2a4f7
Andreas Ahman <andreas.ahman@ikea.com>
2017-07-11 16:40:13 +0200

    Changed so I only have one shelf component in bookshelves

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -10,65 +10,33 @@ class Bookshelf extends Component {
   }
 
   render () {
+    let shelves = ["currentlyReading", "wantToRead", "read"]
+    let shelveNames = ["Currently Reading", "Want To Read", "Read"]
     return (
       <div>
-        <div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {this.props.booksOnShelf.sort(sortBy('title'))
-                  .filter(book => book.shelf === "currentlyReading")
-                  .map(book => (
-                    <Book 
-                      onMoveBook={this.props.onMoveBook}
-                      key={book.id}
-                      book={book}
-                    />
-                  ))
-                }
-              </ol>
+        {shelves.map((shelf, index) => {
+          return(
+            <div key={index}>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">{shelveNames[index]}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {this.props.booksOnShelf.sort(sortBy('title'))
+                      .filter(book => book.shelf === shelf)
+                      .map(book => (
+                        <Book 
+                          onMoveBook={this.props.onMoveBook}
+                          key={book.id}
+                          book={book}
+                        />
+                      ))
+                    }
+                  </ol>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Want To Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {this.props.booksOnShelf.sort(sortBy('title'))
-                  .filter(book => book.shelf === "wantToRead")
-                  .map(book => (
-                    <Book 
-                      onMoveBook={this.props.onMoveBook}
-                      key={book.id}
-                      book={book}
-                    />
-                  ))
-                }
-              </ol>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-             <div className="bookshelf-books">
-              <ol className="books-grid">
-                {this.props.booksOnShelf.sort(sortBy('title'))
-                  .filter(book => book.shelf === "read")
-                  .map(book => (
-                    <Book 
-                      onMoveBook={this.props.onMoveBook}
-                      key={book.id}
-                      book={book}
-                    />
-                  ))
-                }
-              </ol>
-            </div>
-          </div>
-        </div>
+          )}
+        )}
       </div>
     )
   }
